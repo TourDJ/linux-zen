7101a50249604ed605b971674eea1be0ed46538e
Smallbucket <943744213@qq.com>
2019-07-10 08:17:06 +0800

    Update find-perm.sh

--- shell/find-perm.sh
+++ shell/find-perm.sh
@@ -4,8 +4,9 @@
 ##      Invalid mode '+6000' with find -perm
 ## According to man pages of find perm +mode is deprecated 
 ## use `find / -perm /6000 -type f` instead
-## see: https://askubuntu.com/questions/1126527/invalid-mode-6000-with-find-perm
-## so:
+##  -perm +mode
+##      This is no longer supported (and has been deprecated since 2005).  Use -perm /mode instead
+## 
 
 ## File's  permission  bits are exactly mode (octal or symbolic).
 find . -type f -perm 6000
